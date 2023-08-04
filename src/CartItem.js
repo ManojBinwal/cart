05c1a8a960b3bff478b1437223c95a9b809ba72f
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-04 13:05:11 +0530

    added state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile phone',
+      qty: 1,
+      img: ''
+
+    }
+  }
   render () {
+    const {price, title, qty} = this.state;   //object destructuring - give me item from object here //
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>Phone</div>
-          <div style={ { color: '#777' } }>Rs 999</div>
-          <div style={ { color: '#777' } }>Qty: 1</div>
+          <div style={ { fontSize: 25 } }>{this.state.title}</div>
+          <div style={ { color: '#777' } }>Rs {price}</div>
+          <div style={ { color: '#777' } }>Qty: {qty}</div>
           <div className="cart-item-actions">
             {/* Buttons */}
+            <img alt="icrease" className='action-icons' src = "https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+            <img alt="decrease" className='action-icons' src = "https://cdn-icons-png.flaticon.com/128/992/992683.png" />
+            <img alt="delete" className='action-icons' src = "https://cdn-icons-png.flaticon.com/128/484/484662.png" />
           </div>
         </div>
       </div>
